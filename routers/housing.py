96a9e0ebd93079c32417dc3b36cd5623cf0b3058
Javier Quispe Varillas <jjqv1972@gmail.com>
2026-06-14 21:14:18 -0500

    base43

--- routers/housing.py
+++ routers/housing.py
@@ -34,7 +34,7 @@ def housing_price(data: HousingCreate):
     }
     
 @router.post("/",response_model=HousingResponse)
-def housing_price(data: HousingCreate,db: Session = Depends(get_db)):
+def create_housing(data: HousingCreate,db: Session = Depends(get_db)):
     price = predict_price(data.rooms)
 
     new_housing = Housing(
@@ -51,3 +51,45 @@ def housing_price(data: HousingCreate,db: Session = Depends(get_db)):
 @router.get("/",response_model=list[HousingResponse])
 def get_housing(db: Session = Depends(get_db)):
     return db.query(Housing).all()
+
+@router.get("/{housing_id}", response_model=HousingResponse)
+def get_housing_by_id(housing_id: int, db: Session = Depends(get_db)):
+    housing = db.query(Housing).filter(Housing.id == housing_id).first()
+
+    if not housing:
+        raise HTTPException(status_code=404, detail="Registro no encontrado")
+
+    return housing
+
+@router.put("/{housing_id}", response_model=HousingResponse)
+def update_housing(
+    housing_id: int,
+    data: HousingCreate,
+    db: Session = Depends(get_db)
+):
+    housing = db.query(Housing).filter(Housing.id == housing_id).first()
+
+    if not housing:
+        raise HTTPException(status_code=404, detail="Registro no encontrado")
+
+    price = predict_price(data.rooms)
+
+    housing.rooms = data.rooms
+    housing.price = price
+
+    db.commit()
+    db.refresh(housing)
+
+    return housing
+
+@router.delete("/{housing_id}")
+def delete_housing(housing_id: int, db: Session = Depends(get_db)):
+    housing = db.query(Housing).filter(Housing.id == housing_id).first()
+
+    if not housing:
+        raise HTTPException(status_code=404, detail="Registro no encontrado")
+
+    db.delete(housing)
+    db.commit()
+
+    return {"message": "Registro eliminado correctamente"}
